c2110dd7099ee9e10b388761a5cf654e5e405c44
bryanStv <bryans.oyola@gmail.com>
2024-10-28 09:13:51 +0100

    Product partials

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,5 +14,12 @@ class ProductController extends AbstractController
     public function index(): Response
     {
         return $this->render('product/product.html.twig', []);
+    }
+
+    public function productTemplate(ManagerRegistry $doctrine): Response
+    {
+        $repository = $doctrine->getRepository(Product::class);
+        $product = $repository->findAll();
+        return $this->render('partials/_product.html.twig',compact('product'));
     }
 }
